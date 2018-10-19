c0662775fbfef0e66871ec5205aac929257de8ad
slamSIO <slam@siolla.fr>
2018-10-19 10:32:50 +0200

    GUI - colorama

---
+++ App/src/client/gui/coque.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from colorama import Fore , Back , Style
+
+
+if __name__ == '__main__' :
+	
+	print( Fore.RED + 'pyXouDouQi' ) 
+	print( Style.RESET_ALL )
+	
+	
+
